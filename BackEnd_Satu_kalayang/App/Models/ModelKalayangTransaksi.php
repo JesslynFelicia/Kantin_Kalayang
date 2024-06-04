773c2a349c7c2cdb774040ba71ccca2ae36457a3
lareinalaras <larasswara25@gmail.com>
2024-06-05 02:53:18 +0700

    BE update dari Jes

--- BackEnd_Satu_kalayang/App/Models/ModelKalayangTransaksi.php
+++ BackEnd_Satu_kalayang/App/Models/ModelKalayangTransaksi.php
@@ -9,6 +9,6 @@ class ModelKalayangTransaksi extends Model
 {
     use HasFactory;
     protected $table ='tb_transaksi';
-    protected $primaryKey ='id_transaksi ';
+    protected $primaryKey ='id_transaksi';
     protected $fillable = ['id_menu', 'id_order','id_penjual','tanggal_pemesanan', 'nomor_meja','status_pesanan', 'catatan_pemesan', 'ekstra_menu','pesanan','created_at','updated_at'];
 }
